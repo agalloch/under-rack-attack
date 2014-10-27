7ce4dd0ba85e7fccaff274602b715b39fa905b38
Dimitar Terziev <terziev@fidor.de>
2014-10-27 18:55:40 +0200

    initial commit

---
+++ app/controllers/endpoint_controller.rb
@@ -0,0 +1,6 @@
+class EndpointController < ApplicationController
+  use Rack::Attack # FIXME: no using it yet actually
+
+  def index
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AttackedApp::Application.config.secret_token = '221cacac8eb0d526f29fde134dc81809abcc449ec1c435ddb0130bf344c43c54ef61f46b092ce80bff8913b992dab692c4e3df5f9c64a74013458fa340d03003'
